9a0ac822fabee45e65d533819a5aa7f5180a7077
ismailgin7 <ismailgin7@gmail.com>
2026-06-19 12:30:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -215,10 +215,14 @@ function setType(tipe) {
 
   if (tipe === 'masuk') {
     document.getElementById('btn-masuk').className = 'active-income';
-    document.getElementById('kategori').innerHTML = katMasuk.map(k => `<option value="${k}">${k}</option>`).join('');
+    document.getElementById('kategori').innerHTML =
+      katMasuk.map(k => `<option value="${k}">${k}</option>`).join('');
+
   } else if (tipe === 'keluar') {
     document.getElementById('btn-keluar').className = 'active-expense';
-    document.getElementById('kategori').innerHTML = katKeluar.map(k => `<option value="${k}">${k}</option>`).join('');
+    document.getElementById('kategori').innerHTML =
+      katKeluar.map(k => `<option value="${k}">${k}</option>`).join('');
+
   } else if (tipe === 'transfer') {
     document.getElementById('btn-transfer').className = 'active-transfer';
     document.getElementById('form-utama').style.display = 'none';
